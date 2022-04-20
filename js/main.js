eaa0a0728b9194d3228faad252ce8afade03f7ee
Michael Futral <MichaelFutral01@gmail.com>
2022-04-20 17:11:19 -0400

    Performed requested changes in Pull Request #19

--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
-window.addEventListener('DOMContentLoaded', event => {
+// Navbar shrink function
+let navbarShrink = () => {
+    const navbarCollapsible = document.body.querySelector('#mainNav');
+    if (!navbarCollapsible) {
+        return;
+    }
+    if (window.scrollY === 0) {
+        navbarCollapsible.classList.remove('navbar-shrink')
+    } else {
+        navbarCollapsible.classList.add('navbar-shrink')
+    }
+};
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+// Activate Bootstrap scrollspy on the main nav element
+const mainNav = document.body.querySelector('#mainNav');
+if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+        target: '#mainNav',
+        offset: 74,
+    })
+};
 
-    };
+// Shrink the navbar
+navbarShrink();
 
-    // Shrink the navbar
-    navbarShrink();
+// Shrink the navbar when page is scrolled
+document.addEventListener('scroll', navbarShrink);
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-});
